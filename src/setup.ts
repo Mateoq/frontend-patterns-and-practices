ef69d279b4a7dff2eba743ee79b5bc4cc487d0bc
baldore <acornssn@gmail.com>
2017-10-13 15:23:31 -0500

    Add module initialization function

---
+++ src/setup.ts
@@ -0,0 +1,61 @@
+/**
+ * setup.ts
+ *
+ * In this file we initialize everything we need when the page is ready. This
+ * file is really important because it initializes every module and other useful
+ * stuff that you may need to use.
+ */
+
+const MODULE_NAME_ATTR = 'data-module';
+const MODULE_SELECTOR = `[${MODULE_NAME_ATTR}]`;
+
+interface Modules {
+   [key: string]: {
+     default: (elem: HTMLElement) => void
+   }
+ }
+
+/**
+ * Returns the name of the module
+ */
+function getModuleNameFromPath(path: string): string {
+  return path.split('/')[1];
+}
+
+/**
+ * Requires all the modules dynamically and returns an object mapping them.
+ */
+function getModulesObject(): Modules {
+  // WARNING: Don't use variables here!!! require.context works in a very special way so
+  // every param MUST BE a literal. See https://webpack.js.org/guides/dependency-management/#require-context
+  // for more information.
+  const requireContext = (require as any).context('./modules', true, /\.ts$/);
+  return requireContext.keys().reduce((acc, key) => {
+    return {
+      ...acc,
+      [getModuleNameFromPath(key)]: requireContext(key)
+    };
+  }, {});
+}
+
+/**
+ * Initializes all the modules.
+ */
+function initializeModules() {
+  const modules = getModulesObject();
+  const domModules = Array.from(document.querySelectorAll(MODULE_SELECTOR)) as HTMLElement[];
+
+  domModules.forEach((domElement) => {
+    const moduleName = domElement.getAttribute(MODULE_NAME_ATTR);
+
+    if (modules.hasOwnProperty(moduleName)) {
+      modules[moduleName].default(domElement);
+    } else {
+      console.warn(`Module with name ${moduleName} doesn't exist. Check the data-module property of the element: `, domElement);
+    }
+  });
+}
+
+(function runSetup() {
+  initializeModules();
+})();
